afeefaf85624a786382b669b0383a01097d26d64
AustinLeeGordon <gordon.austin2@gmail.com>
2019-01-30 08:59:06 -0600

    Added xmlToJson

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const jsonToCsv = require("./jsonToCsv/jsonToCsv");
+const xmlToJson = require("./xmlToJson/xmlToJson");
 
 module.exports = {
-  jsonToCsv
+  jsonToCsv,
+  xmlToJson
 };
---
+++ src/xmlToJson/xmlToJson.js
@@ -0,0 +1,20 @@
+const xml2js = require("xml2js");
+
+/**
+ * Converts a XML string to JSON
+ * @public
+ * @param {string} xml XML to be converted
+ * @param {object} opts Options for xml2js parser
+ * @returns {json}
+ */
+
+function xmlToJson(xml, opts) {
+  return new Promise((resolve, reject) => {
+    xml2js.parseString(xml, opts, (err, res) => {
+      if (err) return reject(err);
+      resolve(JSON.stringify(res));
+    });
+  });
+}
+
+module.exports = xmlToJson;
